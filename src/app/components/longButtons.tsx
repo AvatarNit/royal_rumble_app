145a22f0c6bcf5fe053728b0b3f5a803bbcf8c47
nicosuriano <surianic000@hsestudents.org>
2025-10-23 13:50:25 -0400

    added admin and long buttons

---
+++ src/app/components/longButtons.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React from "react";
+export default function LongButtons({
+  children,
+  link = "#",
+}: {
+  children: React.ReactNode;
+  link?: string;
+}) {
+  const buttonStyle = {
+    backgroundColor: "#01539c",
+    color: "white",
+    fontFamily: "Poppins, sans-serif",
+    fontWeight: "bold",
+    border: "none",
+    borderRadius: "14px",
+    padding: "10px 40px",
+    width: "160px",
+    textAlign: "center" as const,
+    cursor: "pointer",
+    transition: "background-color 0.3s",
+  };
+
+  const buttonHover = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.currentTarget.style.backgroundColor = "#d10f41";
+  };
+
+  const buttonUnhover = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.currentTarget.style.backgroundColor = "#01539c";
+  };
+
+  return (
+    <button
+      style={buttonStyle}
+      onMouseEnter={(e: React.MouseEvent<HTMLButtonElement>) => buttonHover(e)}
+      onMouseLeave={(e: React.MouseEvent<HTMLButtonElement>) =>
+        buttonUnhover(e)
+      }
+      onClick={() => {
+        if (link) {
+          window.location.href = link;
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+}

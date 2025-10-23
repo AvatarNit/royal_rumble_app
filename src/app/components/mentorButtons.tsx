4a622a2a8165aa62ab7f2a2761c022aef6d9df8f
nicosuriano <surianic000@hsestudents.org>
2025-10-23 08:49:07 -0400

    mentor button test

---
+++ src/app/components/mentorButtons.tsx
@@ -0,0 +1,35 @@
+
+import React from 'react';
+export default function MentorButtons({ children }: { children: React.ReactNode }) {
+  const buttonStyle = {
+    backgroundColor: "#01539c",
+    color: "white",
+    fontFamily: "Poppins, sans-serif",
+    fontWeight: "bold",
+    border: "none",
+    borderRadius: "14px",
+    padding: "40px 20px",
+    width: "160px",
+    textAlign: "center" as const,
+    cursor: "pointer",
+    transition: "background-color 0.3s",
+  };
+
+  const buttonHover = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.currentTarget.style.backgroundColor = "#d10f41";
+  };
+
+  const buttonUnhover = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.currentTarget.style.backgroundColor = "#01539c";
+  };
+
+  return (
+    <button
+      style={buttonStyle}
+      onMouseEnter={(e: React.MouseEvent<HTMLButtonElement>) => buttonHover(e)}
+      onMouseLeave={(e: React.MouseEvent<HTMLButtonElement>) => buttonUnhover(e)}
+    >
+      {children}
+    </button>
+  );
+}

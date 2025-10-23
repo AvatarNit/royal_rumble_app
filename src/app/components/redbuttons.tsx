a0d959fb16578afaf48bd87232091d9195e0e7d5
nicosuriano <surianic000@hsestudents.org>
2025-10-23 14:03:05 -0400

    added red buttons

---
+++ src/app/components/redbuttons.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React from "react";
+export default function redButtons({
+  children,
+  link = "#",
+}: {
+  children: React.ReactNode;
+  link?: string;
+}) {
+  const buttonStyle = {
+    backgroundColor: "#d10f41",
+    color: "white",
+    fontFamily: "Poppins, sans-serif",
+    fontWeight: "bold",
+    border: "none",
+    borderRadius: "14px",
+    padding: "40px 20px",
+    width: "160px",
+    textAlign: "center" as const,
+    cursor: "pointer",
+    transition: "all 0.3s ease",
+  };
+
+  const buttonHover = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.currentTarget.style.backgroundColor = "white";
+    e.currentTarget.style.color = "#d10f41";
+    e.currentTarget.style.border = "2px solid #d10f41";
+  };
+
+  const buttonUnhover = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.currentTarget.style.backgroundColor = "#d10f41";
+    e.currentTarget.style.color = "white";
+    e.currentTarget.style.border = "none";
+  };
+
+  return (
+    <button
+      style={buttonStyle}
+      onMouseEnter={(e: React.MouseEvent<HTMLButtonElement>) => buttonHover(e)}
+      onMouseLeave={(e: React.MouseEvent<HTMLButtonElement>) =>
+        buttonUnhover(e)
+      }
+      onClick={() => {
+        if (link) {
+          window.location.href = link;
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+}

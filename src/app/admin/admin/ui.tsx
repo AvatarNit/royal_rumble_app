4604135d593b28e8a7295b4528dd4ad2abaa6e79
AvatarNit <sajjanithik@icloud.com>
2025-12-05 22:02:44 -0500

    split admin into page.tsx and ui.tsx

---
+++ src/app/admin/admin/ui.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import "bootstrap-icons/font/bootstrap-icons.css";
+import LogoButton from "../../components/logoButton";
+import LoginButton from "../../components/loginButton";
+import EditTable from "../../components/editTable";
+import AddButton from "../../components/addButton";
+import BackButton from "../../components/backButton";
+import "../../css/admin.css";
+import "../../css/logo+login.css";
+import { deleteAdminById } from "@/actions/admin";
+
+export default function AdminAdmin({
+  adminsData,
+}: {
+  adminsData: Array<{
+    adminId: number;
+    fName: string;
+    lName: string;
+    email: string;
+  }>;
+}) {
+  return (
+    <main className="admin-container">
+      <LogoButton />
+      <LoginButton />
+
+      <header className="admin-header">
+        <h1 className="admin-title">Admin Information</h1>
+      </header>
+
+      <BackButton href="/admin" />
+
+      <div
+        style={{
+          width: "85%",
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: "center",
+          marginTop: "20px",
+        }}
+      >
+        <AddButton href="/admin/add/admin">
+          Add
+          <i
+            className="bi bi-plus-circle"
+            style={{ marginLeft: "30px", fontSize: "30px" }}
+          ></i>
+        </AddButton>
+      </div>
+
+      <div style={{ width: "85%", marginTop: "25px" }}>
+        <EditTable
+          headers={["ID", "First Name", "Last Name", "Email"]}
+          data={adminsData.map((a) => [
+            a.adminId ?? "",
+            a.fName ?? "",
+            a.lName ?? "",
+            a.email ?? "",
+          ])}
+          visibleColumns={[1, 2, 3]}
+          editLink="/admin/edit/admin"
+          deleteAction={(id) => deleteAdminById(Number(id))}
+          idIndex={0}
+        />
+      </div>
+    </main>
+  );
+}

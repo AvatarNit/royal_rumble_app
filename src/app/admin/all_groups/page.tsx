0bfe027ce2a549cd4095369d73cf8b3d3ad23a39
AvatarNit <sajjanithik@icloud.com>
2026-01-12 09:26:38 -0500

    all groups split ui and page.tsx

--- src/app/admin/all_groups/page.tsx
+++ src/app/admin/all_groups/page.tsx
@@ -1,5 +1,12 @@
 import AdminAllGroups from "./ui";
+import { getAllGroups } from "@/actions/group";
 
 export default async function AdminPage() {
+  const freshmenGroups = await getAllGroups();
+  const sanitizedFreshmenGroups = freshmenGroups.map((g) => ({
+    groupId: g.groupId,
+    eventOrder: g.eventOrder ?? "[]",
+    routeNum: g.routeNum ?? 0,
+  }));
   return <AdminAllGroups />;
 }

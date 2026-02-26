866bd6bdbfe4e0a09a666cd75fe96b99a5c349da
AvatarNit <sajjanithik@icloud.com>
2026-02-26 09:51:53 -0500

    updated loginButtonSession

--- src/app/components/loginButtonSession.tsx
+++ src/app/components/loginButtonSession.tsx
@@ -6,22 +6,30 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "../css/homepage.css";
 import { useAlert } from "../context/AlertContext";
 
+const DEV_MODE = process.env.DEV_MODE === "true";
+
 export default function LoginButtonSession() {
-  const { data: session } = useSession();
-  const { showAlert } = useAlert();
-  const router = useRouter();
+  if (!DEV_MODE) {
+    const { data: session } = useSession();
+    const { showAlert } = useAlert();
+    const router = useRouter();
 
-  const handleClick = async () => {
-    if (session) {
-      await signOut({ callbackUrl: "/" });
-      showAlert(`Successfully signed out`, "success");
-    } else {
-      router.push("/login");
-    }
-  };
-
+    const handleClick = async () => {
+      if (session) {
+        await signOut({ callbackUrl: "/" });
+        showAlert(`Successfully signed out`, "success");
+      } else {
+        router.push("/login");
+      }
+    };
+    return (
+      <button className="profile-icon-button" onClick={handleClick}>
+        <i className="bi bi-person-fill"></i>
+      </button>
+    );
+  }
   return (
-    <button className="profile-icon-button" onClick={handleClick}>
+    <button className="profile-icon-button">
       <i className="bi bi-person-fill"></i>
     </button>
   );

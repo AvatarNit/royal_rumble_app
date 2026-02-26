ea6cf852edae6c1a7e4f4d2a0929cf9ecb2dbccc
AvatarNit <sajjanithik@icloud.com>
2026-02-26 09:36:53 -0500

    Updated loginButton to log out

--- src/app/components/loginButton.tsx
+++ src/app/components/loginButton.tsx
@@ -1,16 +1,25 @@
 "use client";
 
-import { useCallback } from "react";
+import { useSession, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "../css/homepage.css";
-import { signIn } from "next-auth/react";
 
 export default function LoginButton() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const handleClick = async () => {
+    if (session) {
+      await signOut({ callbackUrl: "/" }); // log out if logged in
+    } else {
+      router.push("/login"); // go to login page if not logged in
+    }
+  };
+
   return (
-    <button className="profile-icon-button">
-      <a href="/login">
-        <i className="bi bi-person-fill"></i>
-      </a>
+    <button className="profile-icon-button" onClick={handleClick}>
+      <i className="bi bi-person-fill"></i>
     </button>
   );
 }

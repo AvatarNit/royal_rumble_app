59061f6baa43a49a9704a6be1281e6f9b84f7fcb
AvatarNit <sajjanithik@icloud.com>
2026-02-26 09:48:18 -0500

    updated loginButtonSession.tsx

--- src/app/components/loginButtonSession.tsx
+++ src/app/components/loginButtonSession.tsx
@@ -4,14 +4,17 @@ import { useSession, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "../css/homepage.css";
+import { useAlert } from "../context/AlertContext";
 
 export default function LoginButtonSession() {
   const { data: session } = useSession();
+  const { showAlert } = useAlert();
   const router = useRouter();
 
   const handleClick = async () => {
     if (session) {
       await signOut({ callbackUrl: "/" });
+      showAlert(`Successfully signed out`, "success");
     } else {
       router.push("/login");
     }

a664c9ba8f54fd6911180e6cfa20eb4b063cd75d
AvatarNit <sajjanithik@icloud.com>
2026-02-26 08:22:12 -0500

    updated top nav bar to be DEV_MODE only

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { AlertProvider } from "./context/AlertContext";
 import AlertMessage from "./components/AlertMessage";
 
 export const dynamic = "force-dynamic";
+const DEV_MODE = process.env.DEV_MODE === "true";
 
 export const metadata: Metadata = {
   title: "Royal Rumble App",
@@ -21,17 +22,19 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <AlertProvider>
-          <nav style={{ textAlign: "center", padding: "30px" }}>
-            <Link href="/">Home</Link> | <Link href="/about">About</Link> |{" "}
-            <Link href="/faq">FAQ</Link> |{" "}
-            <Link href="/freshmen/home">Freshmen</Link> |{" "}
-            <Link href="/mentor/group_leader">Group Leader</Link> |{" "}
-            <Link href="/mentor/hallway_host">Hallway Host</Link> |{" "}
-            <Link href="/mentor/utility_squad">Utility Squad</Link> |{" "}
-            <Link href="/mentor/spirit_session">Spirit Session</Link> |{" "}
-            <Link href="/admin">Admin</Link> |{" "}
-            <Link href="/testPage">Test</Link>
-          </nav>
+          {DEV_MODE && (
+            <nav style={{ textAlign: "center", padding: "30px" }}>
+              <Link href="/">Home</Link> | <Link href="/about">About</Link> |{" "}
+              <Link href="/faq">FAQ</Link> |{" "}
+              <Link href="/freshmen/home">Freshmen</Link> |{" "}
+              <Link href="/mentor/group_leader">Group Leader</Link> |{" "}
+              <Link href="/mentor/hallway_host">Hallway Host</Link> |{" "}
+              <Link href="/mentor/utility_squad">Utility Squad</Link> |{" "}
+              <Link href="/mentor/spirit_session">Spirit Session</Link> |{" "}
+              <Link href="/admin">Admin</Link> |{" "}
+              <Link href="/testPage">Test</Link>
+            </nav>
+          )}
 
           {/* The alert UI that appears at the top of the screen */}
           <AlertMessage />
